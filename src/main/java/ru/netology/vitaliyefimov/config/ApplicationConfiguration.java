b26b7eaf911e5d7c53077963db0cc4bb2f95ac14
Vitaliy <vitaliy-efimov111@mail.ru>
2023-11-25 20:44:42 +0500

    Remove useless classes

--- src/main/java/ru/netology/vitaliyefimov/config/ApplicationConfiguration.java
+++
@@ -1,9 +0,0 @@
-package ru.netology.vitaliyefimov.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("ru.netology.vitaliyefimov.service")
-public class ApplicationConfiguration {
-}
