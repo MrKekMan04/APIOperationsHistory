58daf0e22946ee2edfe10bd0a20dc0a290ff76cf
Vitaliy <vitaliy-efimov111@mail.ru>
2023-11-25 21:03:31 +0500

    Update tests for AsyncInputOperationService

--- src/test/java/ru/netology/vitaliyefimov/service/AsyncInputOperationServiceTest.java
+++ src/test/java/ru/netology/vitaliyefimov/service/AsyncInputOperationServiceTest.java
@@ -1,22 +1,30 @@
 package ru.netology.vitaliyefimov.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import ru.netology.vitaliyefimov.OperationHistoryApiApplicationTest;
+import ru.netology.vitaliyefimov.config.OperationProperties;
+import ru.netology.vitaliyefimov.entity.Currency;
 import ru.netology.vitaliyefimov.entity.Operation;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class AsyncInputOperationServiceTest {
+public class AsyncInputOperationServiceTest extends OperationHistoryApiApplicationTest {
+    @Autowired
+    private AsyncInputOperationService asyncInputOperationService;
+    @Autowired
+    private StatementService statementService;
+    @Autowired
+    private OperationProperties properties;
+
     @Test
     public void assertThatAsyncInputOperationServiceWorksRight() throws InterruptedException {
-        OperationService operationService = new OperationService(new StorageService<>());
-        AsyncInputOperationService asyncInputOperationService = new AsyncInputOperationService(operationService);
-        asyncInputOperationService.startAsyncOperationProcessing();
-        Operation operation = new Operation();
+        Operation operation = new Operation(1, 20, Currency.RUB, "Coffee", 2);
 
         asyncInputOperationService.offerOperation(operation);
-        Thread.sleep(1500);
+        Thread.sleep(2L * properties.getSleepMilliSeconds());
 
-        Operation serviceOperation = operationService.getOperation(0);
+        Operation serviceOperation = statementService.getOperation(operation.getCustomerId(), 0);
         assertEquals(operation, serviceOperation);
         assertEquals(operation.getId(), serviceOperation.getId());
         assertEquals(operation.getSum(), serviceOperation.getSum());
