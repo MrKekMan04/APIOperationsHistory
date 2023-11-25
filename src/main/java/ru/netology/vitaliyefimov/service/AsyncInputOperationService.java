9199a8172df9c2ddc045bba0bb4d48d68abc8382
Vitaliy <vitaliy-efimov111@mail.ru>
2023-11-25 21:03:25 +0500

    Update AsyncInputOperationService

--- src/main/java/ru/netology/vitaliyefimov/service/AsyncInputOperationService.java
+++ src/main/java/ru/netology/vitaliyefimov/service/AsyncInputOperationService.java
@@ -1,27 +1,32 @@
 package ru.netology.vitaliyefimov.service;
 
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.netology.vitaliyefimov.config.OperationProperties;
 import ru.netology.vitaliyefimov.entity.Operation;
 
 import java.util.LinkedList;
 import java.util.Queue;
 
 @Service
+@RequiredArgsConstructor
 public class AsyncInputOperationService {
     private final Queue<Operation> queue = new LinkedList<>();
-    private final OperationService operationService;
-
-    public AsyncInputOperationService(OperationService operationService) {
-        this.operationService = operationService;
-    }
+    private final StatementService statementService;
+    private final OperationProperties properties;
 
     public boolean offerOperation(Operation operation) {
         return queue.offer(operation);
     }
 
-    public void startAsyncOperationProcessing() {
-        Thread t = new Thread(this::processQueue);
-        t.start();
+    @PostConstruct
+    private void postInit() {
+        this.startAsyncOperationProcessing();
+    }
+
+    private void startAsyncOperationProcessing() {
+        new Thread(this::processQueue).start();
     }
 
     private void processQueue() {
@@ -31,14 +36,14 @@ public class AsyncInputOperationService {
             if (operation == null) {
                 try {
                     System.out.println("Waiting for next operation in queue");
-                    Thread.sleep(1_000);
+                    Thread.sleep(properties.getSleepMilliSeconds());
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
             } else {
                 System.out.println("Processing operation:" + operation);
 
-                operationService.addOperation(operation);
+                statementService.setOperation(operation.getCustomerId(), operation);
             }
         }
     }
