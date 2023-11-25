164247eb9146769310015f998496df50748fe847
Vitaliy <vitaliy-efimov111@mail.ru>
2023-11-25 21:01:15 +0500

    Add default test

--- src/test/java/ru/netology/vitaliyefimov/OperationHistoryApiApplicationTest.java
+++ src/test/java/ru/netology/vitaliyefimov/OperationHistoryApiApplicationTest.java
@@ -1,9 +1,6 @@
 package ru.netology.vitaliyefimov;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 public class OperationHistoryApiApplicationTest {
